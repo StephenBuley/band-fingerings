1233f18678d1aa4b97a5f5ca60e273e767f415f4
Stephen Buley <stephen.a.buley@gmail.com>
2023-04-04 15:57:34 +0200

    update fingerings.ts and move types to types.ts

--- src/fingerings.ts
+++ src/fingerings.ts
@@ -1,23 +1,4 @@
-export type TFrenchHornFingerings = {
-  "": string[]
-  "1": string[]
-  "2": string[]
-  "3": string[]
-  "12": string[]
-  "23": string[]
-  "123": string[]
-  T: string[]
-  T1: string[]
-  T2: string[]
-  T3: string[]
-  T12: string[]
-  T23: string[]
-  T123: string[]
-}
-
-export type TFingerings = {
-  frenchHorn: TFrenchHornFingerings
-}
+import { TFingerings } from "./types"
 
 export const fingerings: TFingerings = {
   frenchHorn: {
