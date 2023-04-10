26456d7658ab610efc5e0e5546711e3726e5a07b
Stephen Buley <stephen.a.buley@gmail.com>
2023-04-10 22:08:25 +0200

    update fingerings and pics for C scale

--- src/fingerings.ts
+++ src/fingerings.ts
@@ -2,7 +2,7 @@ import { TFingerings } from "./types"
 
 export const fingerings: TFingerings = {
   frenchHorn: {
-    "": ["C4", "E4", "G4", "Fb4"], // this is for open fingerings
+    "": ["C4", "E4", "G4"], // this is for open fingerings
     "1": ["D4", "F4"],
     "2": ["Eb4", "Gb4"],
     "3": [],
@@ -12,7 +12,7 @@ export const fingerings: TFingerings = {
     "123": [],
     T: [],
     T1: ["Bb4"],
-    T2: ["B4", "Cb4"],
+    T2: ["B4"],
     T3: ["A4"],
     T12: ["A4"],
     T23: ["Ab4"],
