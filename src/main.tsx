4ef09eb77dadb390c1fc9b849b1a8fac98697dda
Stephen Buley <stephen.a.buley@gmail.com>
2023-04-12 13:16:25 +0200

    add instrument component and react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,21 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/fhorn",
+    element: <div>hello french horns!</div>,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
