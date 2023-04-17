557ab165a08b8189bf859d8b4427b99280da083c
Stephen Buley <stephen.a.buley@gmail.com>
2023-04-17 10:15:48 +0200

    Add C#4 and fix spacing for accidentals
    Added C#4 as a test for sharps, and fixed the spacing on all accidentals
    by adding a dash and a space between the accidental and the note, as
    opposed to just a dash.

--- src/musicNotes.ts
+++ src/musicNotes.ts
@@ -1,5 +1,6 @@
 const musicNoteComponents = {
   b: '',
+  s: '',
   q: '',
   qd: '',
   staff: '',
@@ -49,10 +50,11 @@ const musicNotes = {
   trebleD4: encode('d5 q = staff'), // '=', // U+EB9CU+E1D5=U+E01A
   trebleC4: encode('d6 leger d6 q = staff'), // '=', // U+EB9DU+E022U+EB9DU+E1D5=U+E01A
   trebleBb4: encode('b qd staff'), // '', // U+E260 U+E1D5U+E01A
-  trebleAb4: encode('d1 b - d1 q - staff'), // '--', // U+E260 U+E1D5U+E01A
-  trebleDb4: encode('d5 b - d5 q - staff'), // '--', // U+E260 U+E1D5U+E01A
-  trebleEb4: encode('d4 b - d4 q - staff'), // '--', // U+E260 U+E1D5U+E01A
-  trebleGb4: encode('d2 b - d2 q - staff'), // '--', // U+E260 U+E1D5U+E01A
+  trebleAb4: encode('d1 b - _ d1 q - staff'), // '--', // U+E260 U+E1D5U+E01A
+  trebleDb4: encode('d5 b - _ d5 q - staff'), // '--', // U+E260 U+E1D5U+E01A
+  trebleEb4: encode('d4 b - _ d4 q - staff'), // '--', // U+E260 U+E1D5U+E01A
+  trebleGb4: encode('d2 b - _ d2 q - staff'), // '--', // U+E260 U+E1D5U+E01A
+  trebleCs4: encode('d6 s - _ d6 leger d6 q - staff'),
   bassC3: encode('d1 q = staff'),
   bassD3: encode('qd staff'),
   bassE3: encode('u1 qd = staff'),
@@ -61,11 +63,11 @@ const musicNotes = {
   bassA3: encode('u4 qd = staff'),
   bassB3: encode('u5 qd = staff'),
   bassC4: encode('u6 leger u6 qd = staff'),
-  bassAb3: encode('u4 b - u4 qd - staff'),
-  bassBb3: encode('u5 b - u5 qd - staff'),
+  bassAb3: encode('u4 b - _ u4 qd - staff'),
+  bassBb3: encode('u5 b - _ u5 qd - staff'),
   bassDb3: encode('b qd staff'),
-  bassEb3: encode('u1 b - u1 qd - staff'),
-  bassGb3: encode('u3 b - u3 qd - staff'),
+  bassEb3: encode('u1 b - _ u1 qd - staff'),
+  bassGb3: encode('u3 b - _ u3 qd - staff'),
 }
 
 export default musicNotes
